5cf9daf7832cb55702b51a8b42c07cab886aaa55
bweasels <benkumwes@gmail.com>
2025-05-11 10:48:13 -0400

    Refactoring part 2

---
+++ R/clustering.R
@@ -0,0 +1,121 @@
+#' Title
+#'
+#' @param sbird_sce
+#'
+#' @return
+#' @export
+#'
+#' @examples
+identify_subclones <- function(sbird_sce, assay = 'segmented', k = 40, method = inv_manhattan){
+  # Cluster the cells using the changepoint matrix and sce
+  change_mtx <- generate_changepoint_matrix(SummarizedExperiment::assay(sbird_sce, assay), use_mask = SummarizedExperiment::rowData(sbird_sce)$overlap_use)
+  SingleCellExperiment::reducedDim(sbird_sce, paste0(assay, '_changepoint')) <- change_mtx
+
+  # Make graph
+  knn_graph <- RANN::nn2(change_mtx, k = k)
+  knn_matrix <- knn_graph$nn.idx
+
+  # perform clustering
+  membership <- clustering(knn_matrix = knn_matrix, feat_matrix = change_mtx, method = method)
+  sbird_sce$subclone <- membership
+  return(sbird_sce)
+}
+
+# Apply a gaussian kernel to the change matrix
+#' Title
+#'
+#' @param matrix the inputted matrix to smooth over
+#' @param n_neighbors number of adjacent bins to smooth with
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gauss_kernel <- function(matrix, n_neighbors){
+  # Check that n_neighbors is odd
+  if(n_neighbors %% 2 == 0){
+    stop('n_neighbors must be odd')
+  }
+
+  # Define a linear gaussian kernel
+  kernel <- c()
+  for(i in 1:n_neighbors){
+    kernel <- c(kernel, stats::dnorm(i, mean = (n_neighbors + 1)/2, sd = n_neighbors/6))
+  }
+  kernel <- kernel/max(kernel)
+  span <- n_neighbors%/%2
+
+  # Apply the kernel to the matrix
+  kernel_mtx <- matrix(0, nrow = nrow(matrix), ncol = ncol(matrix)-span+1)
+  max_idx <- 0
+  for(i in 1:nrow(matrix)){
+    for(j in (span+1):(ncol(matrix)-span)){
+      kernel_mtx[i,j] <- sum(matrix[i, (j-span):(j+span)] * kernel)
+    }
+  }
+  return(kernel_mtx)
+}
+
+#' Title
+#'
+#' @param matrix the inputted matrix to identify changepoints from
+#' @param use_mask bins to use
+#'
+#' @return
+#' @export
+#'
+#' @examples
+generate_changepoint_matrix <- function(matrix, use_mask){
+  # Find the change points in the mixed matrix
+  matrix <- t(matrix)[,use_mask]
+  change_mtx <- t(apply(matrix, 1, diff))
+
+  # Smooth the change matrix by applying a gaussian kernel
+  kernel_mtx <- round(gauss_kernel(change_mtx, 5), 2)
+
+  # Select change points observed in more than 10% of cells
+  bins_to_use <- which(colSums(abs(kernel_mtx)>0.1)>(nrow(kernel_mtx)*0.1))
+  kernel_mtx <- kernel_mtx[,bins_to_use]
+  return(kernel_mtx)
+}
+
+# inverse manhattan
+inv_manhattan <- function(x, y){
+  # x and y are vectors of the same length
+  l1_dist <- sum(abs(x-y))
+  return(1/(1+l1_dist))
+}
+
+convert_mtxlong <- function(mtx){
+  mtx <- data.frame(mtx)
+  mtx$from <- mtx[,1]
+  mtx_long <- tidyr::pivot_longer(mtx, cols = -from, names_to = 'col', values_to = 'value')
+  return(mtx_long)
+}
+
+clustering <- function(knn_matrix, feat_matrix, method){
+  affy_matrix <- matrix(0, nrow = nrow(knn_matrix), ncol = ncol(knn_matrix))
+  for(i in 1:nrow(knn_matrix)){
+    for(j in 1:ncol(knn_matrix)){
+      neigh_idx <- knn_matrix[i, j]
+      affy_matrix[i,j] <- method(feat_matrix[i,], feat_matrix[neigh_idx,])
+    }
+  }
+
+  affy_long <- convert_mtxlong(affy_matrix)
+  knn_long <- convert_mtxlong(knn_matrix)
+  relations <- data.frame(from = knn_long$from, to = knn_long$value, weight = affy_long$value)
+  graph <- igraph::graph_from_data_frame(relations, directed = F)
+
+  # Perform leiden clustering
+  target <- 150
+  avg_cellsClust <- 0
+  res <- 1
+  while(avg_cellsClust < target & res > 0.1){
+    # Perform clustering
+    clustering <- igraph::cluster_louvain(graph, resolution = res)
+    avg_cellsClust <- nrow(knn_matrix)/length(unique(clustering$membership))
+    res <- res - 0.1
+  }
+  return(clustering$membership)
+}
---
+++ bash/preprocess_sample.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+. env_parallel.bash
+
+# Print a help message
+usage() {                                 # Function: Print a help message.
+    echo "Usage: $0 [ -b BAM File ] [ -o OUTPUT DIRECTORY ] [ -e CONTIGS TO EXCLUDE ]" 1>&2
+    }
+
+# Exit errors
+exit_abnormal() {
+    usage
+    exit 1
+}
+
+# Get arguments from the commandline
+while getopts b:o: flag
+do
+    case "${flag}" in
+	b)
+	    BAM=${OPTARG}
+	    ;;
+	o)
+	    OUTDIR=${OPTARG}
+	    ;;
+	:)
+	    echo "Error: -${OPTARG} requires an argument"
+	    exit_abnormal
+	    ;;
+	*)
+	    echo "Error: Unknown  argument"
+	    exit_abnormal
+    esac
+done
+
+if [ $OPTIND -ne 5 ]; then
+    echo "Incorrect arguments passed";
+    exit_abnormal
+fi
+
+NUMCORES=36
+
+echo "Bam: $BAM";
+echo "Out Dir: $OUTDIR";
+
+# We need to open many files, so temporarily increase the limit
+ulimit -n 4096
+
+# Generate GFF file and spit out cell IDs
+mkdir $OUTDIR
+mkdir $OUTDIR/individualBams
+
+# Filter bam to remove duplicates to reduce file size
+samtools index -@ $NUMCORES $BAM
+samtools view -@ $NUMCORES -q 30 -f 3 -bF 3072 $BAM > $BAM.filtered
+samtools index -@ $NUMCORES $BAM.filtered
+echo "Filtered BAM for quality and removed duplicates"
+
+# Create list of cells to filter based on the metadata and bins and split
+Rscript /home/bkw2118/DLP-Overlap-Pipeline/EAC_Sample_Pipeline/CellIDGenerator.R $BAM.filtered $OUTDIR/CellIDList.txt
+sinto filterbarcodes -b $BAM.filtered -c  $OUTDIR/CellIDList.txt -p 36 --outdir $OUTDIR/individualBams
+echo "Created individual Bam Files"
+
+sort_ind_bam() {
+    samtools sort -m 10G -T ${2} -n ${1} | \
+	samtools fixmate -rm - - | \
+	samtools sort -m 10G -T ${2} - | \
+	samtools markdup -r - - | \
+	samtools sort -m 10G -T ${2} -n - | \
+	bedtools bamtobed -bedpe -mate1 -i - > ${1}.bedpe
+}
+ 
+# gnu parallel seems to not recognize that our server is 1 thread per cpu, so set cpu limit to half what you expect 
+env_parallel --progress --jobs $(($NUMCORES/2)) sort_ind_bam ::: $(ls $OUTDIR/individualBams/*.bam) ::: $OUTDIR
+echo "Filtered Individual BAMs"
